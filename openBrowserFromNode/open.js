5f301aaee19b07990a7e9484c897a94365edde3b
Steve Calla <callasteven@gmail.com>
2023-08-20 15:42:06 -0600

    Open browser from node code

---
+++ openBrowserFromNode/open.js
@@ -0,0 +1,32 @@
+//node open.js "https://bootcampspot.instructure.com/courses/4139"
+//source: https://www.educative.io/answers/how-to-open-a-url-in-the-browser-automatically-with-node
+//To run: Type "node open.js http://www.opencanvas.co.uk"
+
+const { platform } = require('os');
+const { exec } = require('child_process');
+
+const WINDOWS_PLATFORM = 'win32';
+const MAC_PLATFORM = 'darwin';
+
+const osPlatform = platform();
+const args = process.argv.slice(2);
+const [url] = args;
+
+if (url === undefined) {
+  console.error('Please enter a URL, e.g. "http://www.opencanvas.co.uk"');
+  process.exit(0);
+}
+
+let command;
+
+if (osPlatform === WINDOWS_PLATFORM) {
+  command = `start microsoft-edge:${url}`;
+} else if (osPlatform === MAC_PLATFORM) {
+  command = `open -a "Google Chrome" ${url}`;
+} else {
+  command = `google-chrome --no-sandbox ${url}`;
+}
+
+console.log(`executing command: ${command}`);
+
+exec(command);
